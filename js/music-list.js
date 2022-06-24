0e7cef5955c95020c6b206d7773cff05621043a5
Alvaro Cuba <76538507+DjAlvaroCuba@users.noreply.github.com>
2022-06-24 18:23:03 -0500

    en mejora

---
+++ js/music-list.js
@@ -0,0 +1,23 @@
+
+let allMusic = [
+    {
+      name: "Paris",
+      artist: "Morat, Duki",
+      img: "Morat, Duki - París",
+      src: "Morat, Duki - París"
+    },
+    {
+      name: "Mi suerte",
+      artist: "Morat",
+      img: "artworks-e8fOdaCNr1kR-0-t500x500",
+      src: "Mi Suerte"
+      
+    },
+    {
+      name: "Cuando nadie ve",
+      artist: "Morat",
+      img: "Morat - Cuando Nadie Ve",
+      src: "Morat - Cuando Nadie Ve"
+    },
+ 
+  ];
